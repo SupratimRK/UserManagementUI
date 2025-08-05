f62dad147455b1afe867509d8a4d1e82977e4deb
Supratim Mondal <supratimrk@gmail.com>
2025-08-05 13:45:48 +0530

    feat: implement theme provider and mode toggle component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import { UserManagementPortal } from '@/components/UserManagement/UserManagementPortal';
+import { ModeToggle } from '@/components/mode-toggle';
 import './App.css';
 
 function App() {
   return (
     <div className="min-h-screen bg-background">
+      <div className="absolute top-4 right-4">
+        <ModeToggle />
+      </div>
       <UserManagementPortal />
     </div>
   );
